3222a899731963fb4ba62b88da3fc651b1407821
DESKTOP-4B87BA3\Admin <tahirhussain858@gmail.com>
2025-08-12 23:00:48 +0500

    Revert "Work at Upload, Extract,Parse using ML.net, and create separate services for pdf,docs and images, save extracted information in db,add models,dtos, etc"
    This reverts commit 177e33abb3b4b31374e7e660485f7b0b2c05aa81.

--- CareerBuilder.API/Models/TextExtractionResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CareerBuilder.API.Models
-{
-    public sealed class TextExtractionResult
-    {
-        public string Text { get; init; } = string.Empty;
-        public string? Language { get; init; }
-        public double? OcrConfidence { get; init; } // average or heuristic
-    }
-}
--- CareerBuilder.Dto/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CareerBuilder.Dto
-{
-    public class Class1
-    {
-
-    }
-}
